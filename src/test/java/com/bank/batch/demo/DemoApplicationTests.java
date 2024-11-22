1d6c821b5b341509bf8670afc289c66f8f5c71e7
woonie155 <woonie155@gmail.com>
2024-11-23 06:38:06 +0900

    init

---
+++ src/test/java/com/bank/batch/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.batch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
